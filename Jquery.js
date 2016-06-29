e3f0656b17aa59880c4bc14fb904ec005b4c0964
YOUR NAMEJesicaGomez <jesica.gomez@udea.edu.co>
2016-06-29 11:52:23 -0500

    miercoles

--- Jquery.js
+++ Jquery.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+   /*mostrar el color del cubo*/
    $("div").mouseenter(function() {
        $("#color").text($(this).css("background-color"));    
        });
+   /*cambiar la clase en el texto*/
+   $("#dos").addClass("myclass");
+   $("#tres").addClass("highlight");
+   /*poner subrayado en el texto al darle click*/
+   $(".r").click(function() {
+       ($(this).toggleClass("highlight"));
+     });
+   /*un texto diferente para click y doble click*/
+   $("#cuatro").click(function() { 
+       $("#text").text("This is a click Event");
+   });
+   $("#cuatro").dblclick(function() {
+       $("#text").text("This is a double-click Event");
+   });
+   
  });
